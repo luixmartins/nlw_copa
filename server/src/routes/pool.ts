b35ac5ae0b70f4e52f6cea7dc3db66ff02389881
Luiz Martins <luizmartins.uemg@gmail.com>
2022-11-03 19:44:48 -0300

    update server, separate route files

---
+++ server/src/routes/pool.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../lib/prisma"
+import ShortUniqueId from 'short-unique-id'
+import { z } from 'zod'
+
+export async function poolRoutes (fastify: FastifyInstance) {
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+        
+        return {count}
+    })
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(), 
+        })
+        
+        const { title } = createPoolBody.parse(request.body)
+        
+        const generateCode = new ShortUniqueId({length: 6})
+        const code = String(generateCode()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title, 
+                code 
+            } 
+        })
+
+        return reply.status(201).send({title, code})
+    })
+}
+
