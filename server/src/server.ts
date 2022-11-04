bc9ac10aaf04a4c3630a463809712fe638135f2e
Luiz Martins <luizmartins.uemg@gmail.com>
2022-11-03 22:47:52 -0300

    finish backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { poolRoutes } from './routes/pool'
 import { userRoutes } from './routes/user'
@@ -18,13 +19,18 @@ async function bootstrap() {
         origin: true,
     })
 
+    //COLOCAR EM VARIAVEL DE AMBIENTE 
+    await fastify.register(jwt, {
+        secret: 'nlwcopa', 
+    })
+
     await fastify.register(poolRoutes)
     await fastify.register(userRoutes)
     await fastify.register(gameRoutes)
     await fastify.register(guessRoutes)
     await fastify.register(authRoutes)
 
-    await fastify.listen({port: 3333, /*host: '0.0.0.0'*/})
+    await fastify.listen({port: 3333, host: '0.0.0.0'})
 }
 
 bootstrap()
